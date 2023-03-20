5c1416ac608bb5956462bcea3726dbdb5d7d5784
Bayangmbe Mounmo <bayangp0@gmail.com>
2023-03-20 05:47:51 +0100

    modified promt template

--- template_block/_prompt.py
+++ template_block/_prompt.py
@@ -1,2 +1,2 @@
 def structure(context, query):
-    return "Generate the answer to the query from the following context in the given language:\nContext: {}\nQuery: {}\nAnswer:".format(context, query)
+    return "Generate the answer to the query from the following context:\nContext: {}\nQuery: {}\nAnswer:".format(context, query)
